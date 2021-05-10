1f1b275d5b895ce8510ed18ce4a94ae84802ebf7
obed-peralta <adanobedperaltarojas@gmail.com>
2021-05-10 15:15:40 -0500

    Responsive Movil Terminado

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var btnMenuState = false;
+document.getElementById('btnMenu').addEventListener('click',function(){
+    if(btnMenuState){
+        ocultarNav();
+        btnMenuState = false;
+    }else{
+        mostrarNav();
+        btnMenuState = true;
+    }
+});
+
+function mostrarNav(){
+    var nav = document.getElementsByTagName('nav');
+    var header = document.getElementsByTagName('header');
+    nav[0].style.display = 'block';
+    header[0].style.height = '10em';
+}
+function ocultarNav(){
+    var nav = document.getElementsByTagName('nav');
+    var header = document.getElementsByTagName('header');
+    nav[0].style.display = 'none';
+    header[0].style.height = '3.5em';
+}
+function restaurarNav(){
+    var nav = document.getElementsByTagName('nav');
+    var header = document.getElementsByTagName('header');
+    nav[0].style.display = 'block';
+    header[0].style.height = '3.5em';
+}
+function io(val){
+    console.log(val);
+}
+
+window.addEventListener('resize',function(){
+    if(window.screen.width > 480){
+        restaurarNav();
+    }else{
+        ocultarNav();
+    }
+});
